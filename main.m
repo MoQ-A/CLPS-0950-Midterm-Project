50c7a0e0717dc5935e27613a48948ac83dabf0d8
Mo <127126371+MoQ-A@users.noreply.github.com>
2023-03-10 14:52:42 -0500

    Added: 1. update_board script (empty) 2. update-UI script (empty) 3. Updated create_board that creates a game board with different dimensions but same visual size (pixel size changes to keep overall size of the image the same)

--- main.m
+++ main.m
@@ -4,7 +4,9 @@ board_columns = input('Number of columns? \n');
 
 % Create game board
 game_board = create_board(board_rows,board_columns);
-imshow(game_board,[0 255]);
-colorbar;
-colormap rgb
+imshow(game_board, [0 255],'InitialMagnification','fit');
 axis off; axis image;
+
+game_board(10:20,10:20,:) = 0;
+imshow(game_board);
+truesize([400 400]);
