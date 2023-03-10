65b937057d39382cae00fb0a4fda65ff927acef7
Mo <127126371+MoQ-A@users.noreply.github.com>
2023-03-10 13:41:54 -0500

    WIP

--- create_board.m
+++ create_board.m
@@ -1,5 +1,6 @@
 function [game_board] = create_board(rows,columns)
 % create_board creates the default board of customizable size with all white spaces
 %   The game board dimensions are determined from input variables 
-game_board = zeros(rows,columns);
+chan_img = 255*ones(rows,columns);
+game_board = cat(3, chan_img,chan_img,chan_img);
 end
--- main.m
+++ main.m
@@ -1,2 +1,10 @@
-board_rows = input('Number of rows?');
-board_columns = input('Number of columns?');
+% Get game board dimensions from user
+board_rows = input('Number of rows? \n');
+board_columns = input('Number of columns? \n');
+
+% Create game board
+game_board = create_board(board_rows,board_columns);
+imshow(game_board,[0 255]);
+colorbar;
+colormap rgb
+axis off; axis image;
