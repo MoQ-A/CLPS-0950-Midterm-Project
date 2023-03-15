6170491acb8e6d61182407b6970642dbd4cae002
Tlane3 <57160871+Tlane3@users.noreply.github.com>
2023-03-15 09:52:24 -0400

    UI Menu & Round display progress
    Worked on the game menu for start, rules, clear, and go to next generation
    -2 hours Terrence

--- update_UI.m
+++ update_UI.m
@@ -4,19 +4,27 @@
 myicon = imread('playbutton.png');
 f = msgbox('Start', "Custom", myicon);
 
-
+%UI Implementation: Starting Menu
+menu_title = "Game of Life";
+menu_opts = ["Start" "Explanation" "Next" "Clear"];
+choice = menu(menu_title,menu_opts);
 %UI implementation for counting the # of cells populated
+
 
 
 %UI: Explanation-Rules button?
 
 %UI 
 
-%UI Implementation for Counting the number of Rounds
+%UI  Counting the number of Generations a frame is on & display.
+ 
+generation = msgbox('Round', '1' );
+while generation_count > 1
+    generation = msgbox('Round', count(generation_count));
+end
 
+%UI Implementation of button to Reset (From Beginning)
 
-
-%UI Implementtion of button to Reset (From Beginning)
 
 
 
