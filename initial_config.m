3bf31da093161eed79b77d0dc6ff7e620368a0a8
Tlane3 <57160871+Tlane3@users.noreply.github.com>
2023-03-22 18:55:49 -0400

    Initial Population Count & Graph- Loop to continue play V. End Game
    - Commented out UpdateUI

--- initial_config.m
+++ initial_config.m
@@ -1,16 +1,16 @@
-function [new_board] = initial_config(blank_board,cluster_count)
+function [new_board] = initial_config(blank_board,cluster_count, density)
 %INITIAL_CONFIG Takes blank game board and generates random starting
 %configuration
 %   Generates a 'cluster_count' number of 2x2 sqaure clusters of cells 
     new_board = blank_board;
-    cell_count = (length(blank_board))^2;
+    grid_count = (length(blank_board))^2;
     kk = 0;
     for ii = 10:5:length(blank_board)-2
         for jj = 5:5:length(blank_board)-2
             % Chance of a given grid unit generating a cell scales
             % with the size of the board (i.e. bigger board equals
             % smaller chance for a given grid unit)
-		    if randi(cell_count) < cell_count/(cluster_count)
+		    if randi(grid_count) < (density*grid_count)/(cluster_count)
                 config_select = randi(4);
                 if config_select == 1
                     new_board(ii:ii+3,jj:jj+3,1:3) = 0;
--- update_UI.m
+++ update_UI.m
@@ -1,13 +1,13 @@
 % Message Window
 % msgbox('Enter values into command line.', 'Popup Window', 'help');
 
-myicon = imread('playbutton.png');
-f = msgbox('Start', "Custom", myicon);
+%myicon = imread('playbutton.png');
+%f = msgbox('Start', "Custom", myicon);
 
 %UI Implementation: Starting Menu
-menu_title = "Game of Life";
-menu_opts = ["Start" "Explanation" "Next" "Clear"];
-choice = menu(menu_title,menu_opts);
+%menu_title = "Game of Life";
+%menu_opts = ["Start" "Explanation" "Next" "Clear"];
+%choice = menu(menu_title,menu_opts);
 %UI implementation for counting the # of cells populated
 
 
@@ -18,10 +18,10 @@ choice = menu(menu_title,menu_opts);
 
 %UI  Counting the number of Generations a frame is on & display.
  
-generation = msgbox('Round', '1' );
-while generation_count > 1
-    generation = msgbox('Round', count(generation_count));
-end
+%generation = msgbox('Round', '1');
+%while generation_count > 1
+ %   generation = msgbox('Round', count(generation_count));
+%end
 
 %UI Implementation of button to Reset (From Beginning)
 
